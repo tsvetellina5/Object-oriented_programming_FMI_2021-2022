069d1913717b3035185de6b104996a4b776ce2d7
Nikolay Nikolaev <96292977+SupPanda9@users.noreply.github.com>
2022-06-14 11:53:48 +0300

    constructor and name of zoo

--- Additional/Teams/Team7/cpp/main.cpp
+++ Additional/Teams/Team7/cpp/main.cpp
@@ -1,10 +1,15 @@
 #include "Zoopark.h"
-
+#include <iostream>
 using namespace std;
+
+const int BUFFER_SIZE = 1024;
 
 int main()
 {
-	Zoopark zoopark;
+	cout << "What is the name of the zoopark? ";
+	char name[BUFFER_SIZE];
+	std::cin.getline(name, BUFFER_SIZE);
+	Zoopark zoopark(name);
 
 	zoopark.menu();
 }
