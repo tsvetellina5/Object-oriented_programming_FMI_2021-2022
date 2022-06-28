8592592ec83ebe09352d4f5fc671ce110875eb9f
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-06-28 16:51:35 +0300

    Update Adapter.cpp

--- Sem. 15/Structural Patterns/Adapter/Adapter.cpp
+++ Sem. 15/Structural Patterns/Adapter/Adapter.cpp
@@ -1,168 +1,58 @@
-/*
- Observer Design Pattern
+//The Target defines the domain-specific interface used by the client code.
+class Target {
+ public:
+  virtual ~Target() = default;
 
- Intent: Lets you define a subscription mechanism to notify multiple objects
- about any events that happen to the object they're observing.
-
- Note that there's a lot of different terms with similar meaning associated
- with this pattern. Just remember that the Subject is also called the
- Publisher and the Observer is often called the Subscriber and vice versa.
- Also the verbs "observe", "listen" or "track" usually mean the same thing.
-*/
-
-#include <iostream>
-#include <list>
-#include <string>
-
-class IObserver
-{
-public:
-  virtual ~IObserver() = default;
-  virtual void Update(const std::string &message_from_subject) = 0;
+  virtual std::string Request() const {
+    return "Target: The default target's behavior.";
+  }
 };
 
-class ISubject
-{
-public:
-  virtual ~ISubject(){};
-  virtual void Attach(IObserver *observer) = 0;
-  virtual void Detach(IObserver *observer) = 0;
-  virtual void Notify() = 0;
+//The Adaptee contains some useful behavior, but its interface is incompatible with the existing client code.
+//The Adaptee needs some adaptation before the client code can use it.
+class Adaptee {
+ public:
+  std::string SpecificRequest() const {
+    return ".eetpadA eht fo roivaheb laicepS";
+  }
 };
 
-// The Subject owns some important state and notifies observers when the state changes.
-class Subject : public ISubject
-{
-public:
-  virtual ~Subject()
-  {
-    std::cout << "Goodbye, I was the Subject." << std::endl;
-  }
+//The Adapter makes the Adaptee's interface compatible with the Target's interface.
+class Adapter : public Target {
+ private:
+  Adaptee *adaptee_;
 
-  // The subscription management methods.
-  void Attach(IObserver *observer) override
-  {
-    list_observer_.push_back(observer);
+ public:
+  Adapter(Adaptee *adaptee) : adaptee_(adaptee) {}
+  std::string Request() const override {
+    std::string to_reverse = this->adaptee_->SpecificRequest();
+    std::reverse(to_reverse.begin(), to_reverse.end());
+    return "Adapter: (TRANSLATED) " + to_reverse;
   }
-
-  void Detach(IObserver *observer) override
-  {
-    list_observer_.remove(observer);
-  }
-
-  void Notify() override
-  {
-    std::list<IObserver *>::iterator iterator = list_observer_.begin();
-    HowManyObserver();
-    while (iterator != list_observer_.end())
-    {
-      (*iterator)->Update(message_);
-      ++iterator;
-    }
-  }
-
-  void CreateMessage(const std::string &message = "Empty")
-  {
-    this->message_ = message;
-    Notify();
-  }
-  void HowManyObserver()
-  {
-    std::cout << "There are " << list_observer_.size() << " observers in the list." << std::endl;
-  }
-
-  /*
-   Usually, the subscription logic is only a fraction of what a Subject can
-   really do. Subjects commonly hold some important business logic, that
-   triggers a notification method whenever something important is about to
-   happen (or after it).
-  */
-
-  void SomeBusinessLogic()
-  {
-    this->message_ = "change message message";
-    Notify();
-    std::cout << "I'm about to do some thing important\n";
-  }
-
-private:
-  std::list<IObserver *> list_observer_;
-  std::string message_;
 };
 
-class Observer : public IObserver
-{
-public:
-  Observer(Subject &subject) : subject_(subject)
-  {
-    this->subject_.Attach(this);
-    std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
-    this->number_ = Observer::static_number_;
-  }
-
-  virtual ~Observer()
-  {
-    std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
-  }
-
-  void Update(const std::string &message_from_subject) override
-  {
-    message_from_subject_ = message_from_subject;
-    PrintInfo();
-  }
-  void RemoveMeFromTheList()
-  {
-    subject_.Detach(this);
-    std::cout << "Observer \"" << number_ << "\" removed from the list." << std::endl;
-  }
-  void PrintInfo() const
-  {
-    std::cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << std::endl;
-  }
-
-private:
-  std::string message_from_subject_;
-  Subject &subject_;
-  static int static_number_;
-  int number_;
-};
-
-int Observer::static_number_ = 0;
-
-void ClientCode()
-{
-  Subject *subject = new Subject;
-  Observer *observer1 = new Observer(*subject);
-  Observer *observer2 = new Observer(*subject);
-  Observer *observer3 = new Observer(*subject);
-  Observer *observer4;
-  Observer *observer5;
-
-  subject->CreateMessage("Hello World! :D");
-  observer3->RemoveMeFromTheList();
-
-  subject->CreateMessage("The weather is hot today! :p");
-  observer4 = new Observer(*subject);
-
-  observer2->RemoveMeFromTheList();
-  observer5 = new Observer(*subject);
-
-  subject->CreateMessage("My new car is great! ;)");
-  observer5->RemoveMeFromTheList();
-
-  observer4->RemoveMeFromTheList();
-  observer1->RemoveMeFromTheList();
-
-  delete observer5;
-  delete observer4;
-  delete observer3;
-  delete observer2;
-  delete observer1;
-  delete subject;
+//The client code supports all classes that follow the Target interface.
+void ClientCode(const Target *target) {
+  std::cout << target->Request();
 }
 
-int main()
-{
-  ClientCode();
+int main() {
+  std::cout << "Client: I can work just fine with the Target objects:\n";
+  Target *target = new Target;
+  ClientCode(target);
+  std::cout << "\n\n";
+  Adaptee *adaptee = new Adaptee;
+  std::cout << "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
+  std::cout << "Adaptee: " << adaptee->SpecificRequest();
+  std::cout << "\n\n";
+  std::cout << "Client: But I can work with it via the Adapter:\n";
+  Adapter *adapter = new Adapter(adaptee);
+  ClientCode(adapter);
+  std::cout << "\n";
+
+  delete target;
+  delete adaptee;
+  delete adapter;
+
   return 0;
 }
